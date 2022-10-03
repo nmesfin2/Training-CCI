b0bbf532eef017f6e1feb3b9c9e8c4291988e2ef
Natnael Tezera <nmexodus870@gmail.com>
2022-10-03 11:06:10 -0700

    keeping track of min on stack until it got removed

--- com/cracking/datastructure/MyStack.java
+++ com/cracking/datastructure/MyStack.java
@@ -1,30 +1,40 @@
 package com.cracking.datastructure;
 
-public class MyStack<T> {
+import java.util.ArrayList;
+
+public class MyStack<T extends Comparable<T>> implements Comparable<T> {
 	
-	private T [] stackArray; 
+	private ArrayList<T>  stackArray; 
 	private int size;
 	private int topIndex;
-	
+	private T min;
 	MyStack(int arrayLength){
 		this.size = arrayLength;
-		this.stackArray = (T[]) new Object[arrayLength];
+		this.stackArray =   new ArrayList<T>(arrayLength);
 		this.topIndex = -1;
+		this.min = null;
 	}
 	
 	MyStack(){
 		this.size = 0;
-		this.stackArray = (T[]) new Object[10];
+		this.stackArray = new ArrayList<T>(10);
 		this.topIndex = -1;
+		this.min = null;
 	}
 	
 	public boolean push(T val) {
-		if(topIndex == stackArray.length - 1) {
+		//System.out.println(stackArray.size());
+		if(topIndex == size - 1) {
 			return false;
 		}else {
 			this.topIndex++;
-			this.stackArray[topIndex] = val;
-			
+			this.stackArray.add(val);
+			if(this.min == null) {
+				this.min = val;
+			}
+			else if(val.compareTo(this.min) < 0){
+				min = val;
+			}
 			return true;
 			
 		}	
@@ -35,19 +45,23 @@ public class MyStack<T> {
 			return false;
 		}else {
 
-			this.stackArray[topIndex] = null;
+			this.stackArray.set(topIndex, null);
 			this.topIndex--;
 			return true;
 		}
 	}
 	public T peek() {
-		return this.stackArray[this.topIndex];
+		return this.stackArray.get(topIndex);
 	}
 	
 	public boolean isEmpty() {
 		return this.topIndex == -1;
 	}
 	
+	
+	public T Min() {
+		return this.min;
+	}
 	
 	public static void main(String [] args) {
 		MyStack<Integer> myStack = new MyStack<Integer>(10);
@@ -58,9 +72,22 @@ public class MyStack<T> {
 		myStack.push(14);
 		myStack.push(15);
 		
+		
+		
 		while(!myStack.isEmpty()) {
 			System.out.println(myStack.peek());
 			myStack.pop();
 		}
+		
+		System.out.println(myStack.Min());
+	}
+
+	@Override
+	public int compareTo(T o) {
+		// TODO Auto-generated method stub
+		if(this.min.compareTo(o) == 1){
+			return 1;
+		}
+		return 0;
 	}
 }
